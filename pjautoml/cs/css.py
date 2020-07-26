9b9998c0d32c69e0c9b42fbcf3181f850856b210
ealcobaca <e.alcobaca@gmail.com>
2020-07-26 01:34:21 -0300

    Add the base Configuration Space class to deal with multiple configurations spaces.

---
+++ pjautoml/cs/css.py
@@ -0,0 +1,13 @@
+from pjautoml.cs.cs import CS
+
+
+class CSS(CS):
+    def __init__(self, *css):
+        # This for-comprehension is done to guarantee that everything received
+        # is a config space.
+        # You could pass a Component, it will be transformed into a Singleton
+        # CS by this for.
+        css = [cs.cs for cs in css]
+
+        super().__init__({'css': css})
+        self.css = css
