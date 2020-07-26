91b75aa6561eba4763fa441d5a3ee074d852a016
ealcobaca <e.alcobaca@gmail.com>
2020-07-26 10:35:15 -0300

    Fix the python support for version 3.8.

--- setup.py
+++ setup.py
@@ -31,8 +31,7 @@ CLASSIFIERS = ['Intended Audience :: Science/Research',
                'Topic :: Software Development',
                'Topic :: Scientific/Engineering',
                'Operating System :: OS Independent',
-               'Programming Language :: Python :: 3.6',
-               'Programming Language :: Python :: 3.7']
+               'Programming Language :: Python :: 3.8']
 
 INSTALL_REQUIRES = [
     'numpy', 'scipy', 'pjml'
