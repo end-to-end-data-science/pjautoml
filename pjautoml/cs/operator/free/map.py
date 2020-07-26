92948dcc75c5af13a363ca9ef0af5d1676e262a9
ealcobaca <e.alcobaca@gmail.com>
2020-07-26 02:18:40 -0300

    Add the container `Map` to deal with configuration space.

---
+++ pjautoml/cs/operator/free/map.py
@@ -0,0 +1,11 @@
+import pjml.stream.transform.map as ml
+from pjautoml.cs.operator.free.container import Container
+
+
+class Map(Container):
+    """TODO."""
+
+    def __init__(self, *args, seed=0, **kwargs):
+        super().__init__(
+            *args, seed=seed, name=ml.Map.__name__, path=ml.Map.__module__, **kwargs
+        )
